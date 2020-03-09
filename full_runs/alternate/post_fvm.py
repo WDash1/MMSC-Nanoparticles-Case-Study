cdbe4b4e19262a472e05fe5e6247c54759065698
Shyam Sundar Sankaran <shyamss.1995@gmail.com>
2020-03-09 10:57:52 +0000

    Minor updates + post-processing scripts for individual models

--- full_runs/alternate/post_fvm.py
+++ full_runs/alternate/post_fvm.py
@@ -15,14 +15,11 @@ for i in range(t_eval.size):
     N   = h5f['N'][:]
     h5f.close()
 
-    pl.plot(R * R0/1e-9, np.sum(sol[3:-3]) * dr * N, label = 'Particle Model')
-    pl.plot(r * R0/1e-9, sol, 'k--', label = 'Distribution Model')
-    
+    pl.plot(r * R0/1e-9, sol, label = 'Distribution Model')
     pl.title('Time = %3.3f'%(t_eval[i] * t0/3600) + ' hours')
     pl.xlabel(r'$r$(in nm)')
     pl.ylabel(r'$f$')
     pl.xlim(0.4 * R0/1e-9, 2.5 * R0/1e-9)
     pl.ylim(0, 6)
-    pl.legend(fontsize = 20)
-    pl.savefig('comparison_images/%04d'%i + '.png', bbox_inches = 'tight')
+    pl.savefig('fvm_images/%04d'%i + '.png', bbox_inches = 'tight')
     pl.clf()
--- full_runs/alternate/post_histogram.py
+++ full_runs/alternate/post_histogram.py
@@ -15,14 +15,14 @@ for i in range(t_eval.size):
     N   = h5f['N'][:]
     h5f.close()
 
-    pl.plot(R * R0/1e-9, np.sum(sol[3:-3]) * dr * N, label = 'Particle Model')
-    pl.plot(r * R0/1e-9, sol, 'k--', label = 'Distribution Model')
-    
+    R = R * R0/1e-9
+    width = 0.7 * (R[1] - R[0])
+
+    pl.bar(R, np.sum(sol[3:-3]) * dr * N, align='center', width=width)
     pl.title('Time = %3.3f'%(t_eval[i] * t0/3600) + ' hours')
     pl.xlabel(r'$r$(in nm)')
     pl.ylabel(r'$f$')
     pl.xlim(0.4 * R0/1e-9, 2.5 * R0/1e-9)
     pl.ylim(0, 6)
-    pl.legend(fontsize = 20)
-    pl.savefig('comparison_images/%04d'%i + '.png', bbox_inches = 'tight')
+    pl.savefig('histogram_images/%04d'%i + '.png', bbox_inches = 'tight')
     pl.clf()
