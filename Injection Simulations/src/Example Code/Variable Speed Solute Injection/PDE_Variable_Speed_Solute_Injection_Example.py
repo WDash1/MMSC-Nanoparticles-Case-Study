90320c60e97b1a71e7f8d2172eb2de2cccb8cef9
William Dash <william.dash@thedashgroup.co.uk>
2020-03-18 18:28:30 +0000

    This update adds additional documentation to the PDE_Variable_Speed_Solute_Injection_Example.py source code file.

--- Injection Simulations/src/Example Code/Variable Speed Solute Injection/PDE_Variable_Speed_Solute_Injection_Example.py
+++ Injection Simulations/src/Example Code/Variable Speed Solute Injection/PDE_Variable_Speed_Solute_Injection_Example.py
@@ -1,10 +1,17 @@
+##  @brief      This file includes the source code that was used to generate
+#               the distribution images for solute injections of different
+#               speeds, whose graphs are shown in slide 33 of the
+#               presentation.
+
 import numpy as NP
 
 import sys
-sys.path.append('../../')
 
-from DataVisualiser import DataVisualiser
+sys.path.append('../../Utilities/')
+
 from PDEModelSimulator import PDEModelSimulator
+from DistributionDataProcessor import DistributionDataProcessor;
+from DistributionDataVisualiser import DistributionDataVisualiser;
 
 #The parameter settings that we wish to use for our simulation.
 r_min = 0.5;
@@ -34,12 +41,21 @@ number_of_functions = 4;
 
 
 #The directory that we wish to write the resulting graph images to.
-output_folder = "Raw_Images/";
+output_folder = "Distribution_Plots/";
+
+#The size of the fonts to be used for the axis and legend labels of the output
+#graphs.
+label_font_size = 13;
+
+#The size of the fonts to be used for the title of the output graphs.
+title_font_size = 17;
 
 
+#This function corresponds to no solute injection.
 def injectionFunction1(t):
     return 0;
 
+#This function corresponds to a linear solute inejection.
 def injectionFunction2(t, start_time, end_time, injection_amount):
     if(t>start_time):
         if(t>=end_time):
@@ -49,7 +65,7 @@ def injectionFunction2(t, start_time, end_time, injection_amount):
     else:
         return 0;
     
-
+#This function corresponds to a quadratic solute injection.
 def injectionFunction3(t, start_time, end_time, injection_amount):
     if(t>start_time):
         if(t>=end_time):
@@ -59,19 +75,18 @@ def injectionFunction3(t, start_time, end_time, injection_amount):
     else:
         return 0;
     
-        
-    
-    
-def injectionFunction6(t, start_time, end_time, injection_amount):
+#This function corresponds to a square root solute injection.
+def injectionFunction4(t, start_time, end_time, injection_amount):
     if(t>start_time):
         if(t>=end_time):
             return injection_amount;
         else:
-            scale = NP.log(0.001);
-            return NP.exp(scale*(end_time-t)/(end_time-start_time))*injection_amount;
+            return NP.sqrt(t-start_time)/(NP.sqrt(end_time-start_time))*injection_amount;
     else:
         return 0;
-
+        
+    
+#This function corresponds to a logarithmic solute injection.
 def injectionFunction5(t, start_time, end_time, injection_amount):
     if(t>start_time):
         if(t>=end_time):
@@ -82,14 +97,17 @@ def injectionFunction5(t, start_time, end_time, injection_amount):
         return 0;
 
 
-def injectionFunction4(t, start_time, end_time, injection_amount):
+#This function corresponds to an exponential solute injection.    
+def injectionFunction6(t, start_time, end_time, injection_amount):
     if(t>start_time):
         if(t>=end_time):
             return injection_amount;
         else:
-            return NP.sqrt(t-start_time)/(NP.sqrt(end_time-start_time))*injection_amount;
+            scale = NP.log(0.001);
+            return NP.exp(scale*(end_time-t)/(end_time-start_time))*injection_amount;
     else:
         return 0;
+
 
     
 
@@ -110,9 +128,22 @@ line_colours = ["black",
                 "yellow",
                 "purple"];
 
-                
+#This array specifies the type of each line on the output graphs.
+line_styles = ["--",
+               "--",
+               "--",
+               "--",
+               "--",
+               "--"];
+
+              
+#This parameter dictates the dimensionalised time (in seconds) that we will
+#start and stop injecting solute (respectively).
 injection_start_time=2000;
 injection_stop_time=5000;
+
+#This parameter dictates the change in concentration $c_{\infty(0)}$ caused by
+#the injection of solute into the reaction.
 injection_amount=50;
                 
 
@@ -129,108 +160,42 @@ injection_functions = [injectionFunction1,
 #that will be used by any such simulation.
 simulation_environment = PDEModelSimulator( end_time, r_min, r_max, 
                                     l_cap, c_inf_0, cs, Vm, D, k, N0, R0)
-                        
+
+#Retrieve the time and r values used in the simulation and scale them
+#appropriately.
 time_values = simulation_environment.getTValues()/3600;
-r_values = simulation_environment.getRValues();
+r_values = simulation_environment.getRValues()*1e9;
 
-                       
-#Create a function that performs numerical simulations of the PDE model for
-#a given injection function index.                       
-numerical_simulator = lambda index: simulation_environment.simulateModel(n0, 
-                                    injection_functions[index]);
-
-#Perform simulations of the PDE model for each different injection function.
-solution_data = list(map(numerical_simulator, 
-                           NP.arange(number_of_functions)));
-
-#Create a DataVisualiser object instance in order to export the numeric
-#results into graphical form.
-visualiser = DataVisualiser(1,   0.4 * R0/1e-9, 7, 'r (in nm)',
-                                 0, 8, 'N(r,t)', 
-                                 line_key_strings[0:number_of_functions],
-                                 line_colours[0:number_of_functions]);
-                          
-                            
-                             
-#For each time step create a graph containing the N distribution produced using
-#each of the different injection functions and export them to an external
-#image file.
-
-mean_values = NP.empty(number_of_functions, dtype=object);
-variance_values = NP.empty(number_of_functions, dtype=object);                 
-
-moment_calculator = lambda n_data, moment_number: NP.dot(
-                n_data, NP.power(r_values, moment_number))/sum(n_data);
-
-
-mean_calculator = lambda n_data: moment_calculator(n_data, 1);
-
-variance_calculator = lambda n_data: moment_calculator(n_data, 2) - mean_calculator(n_data)**2;
-
-
-
-
-
-max_means=list(range(0,number_of_functions));
-min_means=list(range(0,number_of_functions));
-
-max_vars=list(range(0,number_of_functions));
-min_vars=list(range(0,number_of_functions));
-                                 
-
-for i in range(number_of_functions):
-    n_values = list(map(lambda j: solution_data[i][:,j], 
-                        list(range(0,time_values.size))));
-    
-                        
-    mean_values[i] = list(map(lambda j: mean_calculator(solution_data[i][:,j])*1e9, 
-                        list(range(0,time_values.size))));
-               
-    max_means[i] = max(mean_values[i]);
-    min_means[i] = min(mean_values[i]);           
-               
-    variance_values[i] = list(map(lambda j: 1e18 * variance_calculator(solution_data[i][:,j]), 
-                        list(range(0,time_values.size))));
-    max_vars[i] = max(variance_values[i]);
-    min_vars[i] = min(variance_values[i]);
-    
-mean_visualiser = DataVisualiser(1,   min(time_values), max(time_values), 't (in hours)',
-                                 min(min_means)*0.9, max(max_means)*1.1, 'Average r (in nm)', 
-                                 line_key_strings[0:number_of_functions],
-                                 line_colours[0:number_of_functions]);
-                                 
-                                 
-variance_visualiser = DataVisualiser(1,   min(time_values), max(time_values), 't (in hours)',
-                                 min(min_vars)*0.9, max(max_vars)*1.1, 'Variance of r (in nm$^2$)', 
-                                 line_key_strings[0:number_of_functions],
-                                 line_colours[0:number_of_functions]);
-                   
-#add_dataset_function = lambda dataset: map(mean_visualiser.addData(time_values, dataset[i],i), list(range(0,dataset.size)));
-
-
-#add_dataset_function(mean_values);
-
-for i in range(number_of_functions):
-    mean_visualiser.addData(time_values, mean_values[i],i);
-    variance_visualiser.addData(time_values, variance_values[i],i)
-
-mean_visualiser.exportGraph(' ', 'Stats/mean_graph.png', True);
-variance_visualiser.exportGraph(' ', 'Stats/variance_graph.png', True);
-mean_visualiser.clearData();
-variance_visualiser.clearData();
-
-
-               
-#    mean_values[i] = list(map(lambda j: mean_calculator(n_values[j,:]), list(range(0,time_values.size))));
- #   variance_values[i] = list(map(lambda j: variance_calculator(n_values[j,:]), list(range(0,time_values.size))));
           
-for i in range(time_values.size):
-    print('Time =', time_values[i])
+#Put the data into a processor so that we can create graphs an calculate
+#some useful statistics.
+data_processor = DistributionDataProcessor(r_values, time_values, 
+                                           number_of_functions); 
+for j in range(number_of_functions):
+    data = simulation_environment.simulateModel(n0, injection_functions[j]);
+    data_processor.addData(data, j);
 
-    for j in range(number_of_functions):
-        current_N_data = solution_data[j][:,i];    
-        visualiser.addData(1/1e-9*r_values, current_N_data, j);   
+#Create a visualiser for the simulation data.
+data_visualiser = DistributionDataVisualiser("r (in nm)", "N(r,t)", 
+                                             line_key_strings[0:number_of_functions],
+                                             line_colours[0:number_of_functions],
+                                             line_styles[0:number_of_functions],
+                                             label_font_size,
+                                             title_font_size,
+                                             data_processor);   
 
-    visualiser.exportGraph('Time: %3.3f'%(time_values[i]) + ' hours', output_folder+'/%04d'%i + '.png', False);
-    visualiser.clearData();
-    
+                                             
+#Generate a graph of how the mean of the distribution evolves with time.
+data_visualiser.exportMeanTimeGraph(1, "", "t (in hours)", "Average r (in nm)", 
+                                    "./Statistics/mean_graph.png", True,
+                                    True, 0, 0);
+
+#Generate a graph of how the variance of the distirbution evolves with time.
+data_visualiser.exportVarianceTimeGraph(2, "", "t (in hours)", 
+                                        "Variance of r (in nm$^2$)",
+                                        "./Statistics/variance_graph.png",
+                                        True, True, 0, 0);
+
+#Generate graphs of the distribution at each time step in the simulation.                                        
+data_visualiser.exportDistributionGraphAll(output_folder, False, False, 0, 7,
+                                           True, 0, 0);	
